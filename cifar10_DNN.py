abd3f07ff01d16ac149d83f51f161eb9f892b4c6
IzPerfect <yjwan8390@gmail.com>
2019-04-29 00:56:56 +0900

    nothing

--- cifar10_DNN.py
+++ cifar10_DNN.py
@@ -4,6 +4,7 @@ from keras.layers import Dense, Dropout, Flatten, Activation
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 import numpy as np
 from data_utils import *
@@ -45,6 +46,39 @@ class CifarDNN(object):
                           ,metrics = ['accuracy'])
 
         return model
+
+    # train with data augmentation
+    def train_generator(self, X_train, Y_train, epoch = 10, batch_size = 32, val_split = 0.2, save_model = True):
+        self.generator = ImageDataGenerator(
+            featurewise_center = True,
+            featurewise_std_normalization = True,
+            rotation_range = 20,
+            width_shift_range = 0.2,
+            height_shift_range = 0.2
+        )
+
+        X_train = X_train.reshape(X_train.shape[0], 32, 32, 3)
+        self.generator.fit(X_train)
+
+        save_dir = './save_model/'
+        if not os.path.exists(save_dir): # if there is no exist, make the path
+            os.makedirs(save_dir)
+
+        model_path = save_dir + '{epoch:02d}-{val_loss:.4f}-{acc:.4f}.hd5'
+        cb_checkpoint = ModelCheckpoint(filepath = model_path, monitor = 'val_loss',
+            verbose = 1, save_best_only = True)
+
+
+        #X_train = X_train.reshape(-1, 32*32*3)
+
+        self.history_generator = self.model.fit_generator(self.generator.flow(X_train, Y_train,
+                                         batch_size=batch_size),
+                            steps_per_epoch = X_train.shape[0] // batch_size,
+                            epochs = epoch,
+                            callbacks=[cb_checkpoint])
+
+        return self.history_generator
+
 
     # train model
     def train(self, X_train, Y_train, epoch = 10, batch_size = 32, val_split = 0.2, save_model = True):
